a19074144b1825061cbbccf8de27a9249b856093
Ashin Amanulla <ashin.jnv@gmail.com>
2025-04-15 14:47:46 +0400

    test

--- src/controllers/registrations.controller.js
+++ src/controllers/registrations.controller.js
@@ -396,8 +396,9 @@ export const sendOtp = async (req, res) => {
         await Promise.all([
             console.log('sending email',email,contactNumber),
             sendEmail(email, 'OTP Verification for UNMA 2025 Registration',
-                `Your OTP for UNMA 2025 registration is ${otp}. It will expire in 60 minutes.`),
-            sendWhatsAppOtp(contactNumber, otp)]);
+                `Your OTP for UNMA 2025 registration is ${otp}. It will expire in 5 minutes.`),
+            sendWhatsAppOtp(contactNumber, otp)
+        ]);
 
 
         logger.info(`OTP sent to ${email} and ${contactNumber}`);
--- src/utils/whatsapp.js
+++ src/utils/whatsapp.js
@@ -55,4 +55,4 @@ export const sendWhatsAppOtp = async (phoneNumber, otp) => {
     }
 };
 
-// kk
+// kkk
